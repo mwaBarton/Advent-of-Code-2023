bfcd136320a3bb58f98ab238336b38588b50433d
Matthew Arnmor <MWA@barton.ac.uk>
2023-12-01 06:59:06 +0000

    Preparing for Day 2

---
+++ AoC_23/Day 02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_02
+{
+    internal class Program
+    {
+        static string[] GetLines(bool test = false)
+        {
+            return File.ReadAllLines(test ? "test.txt" : "input.txt");
+        }
+
+        static string ArrayToString<T>(T[] array, bool newLine = false)
+        {
+            string result = "[";
+            if (newLine) result += "\n";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i].ToString();
+
+                if (i < array.Length - 1)
+                {
+                    result += ", ";
+                    if (newLine) result += "\n";
+                }
+            }
+
+            if (newLine) result += "\n";
+            result += "]";
+
+            return result;
+        }
+
+        static void Part1()
+        {
+            
+        }
+
+        static void Part2()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Part1();
+
+            Console.ReadKey();
+        }
+    }
+}
